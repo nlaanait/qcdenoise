10f80dbd1cde17f5e66a9d11f300607ee963c141
Numan Laanait <laanaitn@ornl.gov>
2020-02-17 21:23:24 -0500

    Revert "Update circuit_constructors.py -- random sampling of 1, 2,3 qubit subcircuits"
    This reverts commit 2b806980a3e4923f90cdbfbafa7043388eadb248 [formerly e4dfbcc23839a4c45fde7a8c674ebd6394a94628].
    
    
    Former-commit-id: 779dbee637ae0aceb7949c01366bff4ec5114fb3

--- qcdenoise/circuit_constructors.py
+++ qcdenoise/circuit_constructors.py
@@ -7,6 +7,48 @@ class CircuitConstructor:
         """
         pass 
 
+    def _build_from_gate_set(self,gate_set):
+        '''
+        This was adapted from the _build_GHZ() routine in circuit_sampler
+        So I assumed that the object would have similar class attributes
+        self.n_qubits, self.insert_unitary ... etc
+        first draft of building a circuit constructor from a list of gates
+        pass gate_set as a list of dictionaries (yes/no?  other?)
+        
+        [{'gate':'I' or 'H' or 'CNOT','qubits': single or tuple}]
+        ex: ['I','I','I','I'] = [{'gate':'I','qubit':0},{'gate':'I','qubit':1},\
+                                    {'gate':'I','qubit':2},{'gate':'I','qubit':3}]
+        ex (GHZ-3): ['H','CNOT','CNOT'] = [{'gate':'H','qubit':0},{'gate':'CNOT','qubit':(0,1)},\
+                                            {'gate':'CNOT','qubit':(1,2)}]
+        ex (C-3): ['H','CNOT','H','CNOT','H','H'] = [{'gate':'H','qubit':0},{'gate':'CNOT','qubit':(0,1)},\
+                                            {'gate':'H','qubit':1},{'gate':'CNOT',\
+                                            'qubit':(1,2)},{'gate':'H','qubit':0},{'gate':'H','qubit':2}]
+        '''
+    
+        q_reg = QuantumRegister(self.n_qubits)
+        c_reg = ClassicalRegister(self.n_qubits)
+        circ = QuantumCircuit(q_reg, c_reg)
+        if self.insert_unitary:
+            # excite one of the qubits to |1> 
+            idx = np.random.randint(0, self.n_qubits)
+            circ.initialize([0,1], q_reg[idx]) #pylint: disable=no-member
+        self.ops_labels = []
+        for gdx in range(len(gate_set)):
+            if gate_set[gdx]['gate'] is 'H':
+                qbx=gate_set[gdx]['qubit']
+                circ.h(qr[qbx])
+            elif gate_set[gdx]['gate'] is 'CNOT':
+                src,tgt=gate_set[gdx]['qubit']
+                circ.cx(qr[qr[src],qr[tgt])
+                if self.insert_unitary and bool(np.random.choice(2)):
+                    label = 'unitary_{}_{}'.format(q,q+1)
+                    self.ops_labels.append(label)
+                    circ.unitary(self.unitary_op, [q, q+1], label=label)
+            elif gate_set[gdx]['gate'] is 'I':
+                print('identity gate is trivial')
+        circ.measure(q_reg, c_reg) #pylint: disable=no-member
+        return circ
+    
     def build_graph(self):
         """Build a graph from a set of sub-graphs
         """
